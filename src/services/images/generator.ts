822236f6c8b365ae22e23497a85efffb291d9251
Tomohiko Ikeda <tom-e-kid@users.noreply.github.com>
2022-12-14 13:24:56 +0900

    refactor: update puppeteer's configuration

--- src/services/images/generator.ts
+++ src/services/images/generator.ts
@@ -9,14 +9,22 @@ export const generate = async (input: GeneratorInput) => {
     template,
   } = input
   const browser = await puppeteer.launch({
-    defaultViewport: { width, height },
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    args: [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+      '--single-process',
+      '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+    ],
   })
-
-  const html = await buildTemplate(template, input)
-
   const page = await browser.newPage()
+  await page.setViewport({ width, height })
+  const html = await buildTemplate(template, input)
   await page.setContent(html)
   const snapshot = await page.screenshot(
     format === 'jpeg' ? { type: 'jpeg', quality: 80 } : undefined
